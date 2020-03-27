0fb6c168e6ec591cecae55c64f8a9d75aa8907c5
Caio Magalhaes <caio.lmf@gmail.com>
2020-03-27 18:05:24 -0300

    Upload exercise 1 solution

---
+++ exercices/4_2/exercise_1.js
@@ -0,0 +1,14 @@
+let n = 5
+let string = null;
+
+for (let index = 0; index < n; index++) {
+  string = "";
+
+  for (let index = 0; index < n; index++) {
+    string = string + '*';
+    
+  }
+  
+  console.log(string);
+  
+}
